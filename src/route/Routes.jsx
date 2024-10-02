a7371f2cd29faba34b6a98280f5c4ca42edc616c
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-02 14:34:04 +0900

    라우터 링크 완성

--- src/route/Routes.jsx
+++ src/route/Routes.jsx
@@ -8,10 +8,23 @@
 
 // export default Routes
 import React, { Component } from 'react'
+import HomePage from '../pages/HomePage'
+import BoardPage from '../pages/boardPage'
+import ProfilePage from '../pages/ProfilePage'
 // Routes로 들어오면 그 경로에 맞게 결과를 보여줌
-export default [
+
+const routes = [
+  { path: '/', element: <HomePage />, title: 'Home' },
   {
-    path: '/',
-    component: Home,
+    path: '/board',
+    element: <BoardPage />,
+    title: 'Board',
+  },
+  {
+    path: '/profile',
+    element: <ProfilePage />,
+    title: 'profile',
   },
 ]
+
+export default routes
