14641123ed32102b672dfab99454247af4c217df
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-02 11:40:12 +0900

    부트스트랩 적용완료

--- src/App.jsx
+++ src/App.jsx
@@ -2,19 +2,23 @@ import { useState } from 'react'
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
 //import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import BootStrapButton from './BootstrapExample'
+import Header from './componets/Header'
 
 function App() {
+  const hello = '안녕하세요'
 
-  const hello = "안녕하세요";
-  
-  const style ={
-    fontSize:30
+  const style = {
+    fontSize: 30,
   }
 
   return (
     <>
+      <Header></Header>
       <div style={style}>{hello}</div>
       <div>{hello}</div>
+      <BootStrapButton />
     </>
   )
 }
