01c9d094b58af979698b638a0373a1e53903a7fc
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-04 10:48:20 +0900

    프롭스 기본완료

--- src/pages/PropsPage.jsx
+++ src/pages/PropsPage.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 import Hello from '../components/Hello'
+import Hello2 from '../components/Hello2'
 //<Hello name="홍길동">  객체임 그래서 파라미터를 만들어서 넘길수 있다.
 const PropsPage = () => {
   return (
     //prettier-ignore
     <>
       <Hello name="홍길동" color="red" age="20"> </Hello>
-      <Hello name="홍길순" color="blue" age="20"> </Hello>
-      
+
+      <Hello2 name="홍길순" color="blue" age="20"> </Hello2>
     </>
   )
 }
