e5e9fd4f05619ca77e8a6bc557059b3659049a47
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-07 12:45:05 +0900

    성적표

--- src/pages/InputGradePage.jsx
+++ src/pages/InputGradePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import InputGrade from '../components/InputGrade'
+import InputGrade2 from '../components/InputGrade2'
 
 const InputPage = () => {
   //prettier-ignore
   return (
     <div className="text-center mt-5">
-      <InputGrade/>
+      <InputGrade />
+      <hr />
+      <InputGrade2/>
     </div>
   )
 }
