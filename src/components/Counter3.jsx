57e1f849b034ed2276e1ca66ff0f9d3558055c07
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-07 11:34:34 +0900

    성적표

--- src/components/Counter3.jsx
+++ src/components/Counter3.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react'
 // state 사용이유 : 동적인 데이터 화면갱신을 위하여 리액트가 관리하는 것
-const Counter2 = () => {
-  const [number, setNumber] = useState(0)
-  const onIncrease = () => {
-    setNumber(number + 2)
-  }
-  const onDecrease = () => {
-    setNumber(number - 2)
-  }
-  return (
-    //prettier-ignore
-    <div>
-      <h1>{number}</h1>
-      <button onClick={onIncrease}>+2</button>
-      <button onClick={onDecrease}>-2
+const Counter3 = () => {
+  //let text = ''
 
-      </button>
+  const [kor, setKor] = useState('')
+  const [math, setMath] = useState('')
+  const [eng, setEng] = useState('')
+  const [total, setTotal] = useState('')
+  const [avg, setAvg] = useState('')
+
+  const onChange = (event) => {}
+
+  //prettier-ignore
+  return (
+    <div>
+      국어 <input onChange={onChange} value={kor}/>
+      수학 <input onChange={onChange} value={math}/>
+      영어 <input onChange={onChange} value={eng}/>
+      <div>
+        <b>총점 {total} </b>
+        <b>평균 {avg} </b>
+      </div>
     </div>
   )
 }
 
-export default Counter2
+export default Counter3
--- src/pages/InputPage.jsx
+++ src/pages/InputPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
-import Input from '../components/Input'
+import Input from '../components/input'
+import Counter3 from './../components/Counter3'
 
 const InputPage = () => {
   //prettier-ignore
   return (
     <div className="text-center mt-5">
       <Input />
+      <Counter3/>
     </div>
   )
 }
