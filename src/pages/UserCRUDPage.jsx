2f6b8768d9dbb1a1760ffc57174c6d7cdd4e7027
WIN-1NJ543GDPBU\ICT02_04 <rlaaltnr8560@naver.com>
2024-10-10 09:36:38 +0900

    crud

--- src/pages/UserCRUDPage.jsx
+++ src/pages/UserCRUDPage.jsx
@@ -54,11 +54,17 @@ const UserCRUDPage = () => {
     nextId.current += 1
   }
 
+  const onRemove = (id) => {
+    // user.id 가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
+    // = user.id 가 id 인 것을 제거함
+    setUsers(users.filter((user) => user.id !== id))
+  }
+
   //prettier-ignore
   return (
     <div className='text-center mt-5'>
       <CreateUser username={username} email={email} onChange={onChange} onCreate={onCreate}></CreateUser>
-      <UserList users={users}></UserList>
+      <UserList users={users} onRemove={onRemove}></UserList>
     </div>
   )
 }
